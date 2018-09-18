faba83312553c59ec79d4da8de7ef65e418915a2
Fatih Tasdemir <tsdmrfth@gmail.com>
2018-09-18 19:39:55 +0300

    Added dummy screens to test navigator.

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
+import WelcomeScreen from "./screens/WelcomeScreen";
+import AuthScreen from "./screens/AuthScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import ReviewScreen from "./screens/ReviewScreen";
+import SettingsScreen from "./screens/SettingsScreen";
 
 export default class App extends React.Component {
+
   render() {
+      const Navigator = createBottomTabNavigator({
+          welcome: {screen: WelcomeScreen},
+          auth: {screen: AuthScreen},
+          main: {
+              screen: createBottomTabNavigator({
+                  map: {screen: MapScreen},
+                  deck: {screen: DeckScreen},
+                  review: {
+                      screen: createStackNavigator({
+                          review: {screen: ReviewScreen},
+                          settings: {screen: SettingsScreen}
+                      })
+                  }
+              })
+          }
+      });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+          <Navigator/>
       </View>
     );
+
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
