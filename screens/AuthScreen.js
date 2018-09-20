799b78e02d24370d11a4a7a7f5bd2f53af7fc940
Fatih Tasdemir <tsdmrfth@gmail.com>
2018-09-20 22:26:01 +0300

    Added another OAuth methods to application. Added login button animations.

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Image, Text, View} from 'react-native';
+import {Animated, Image, Text, View} from 'react-native';
 import Button from "../components/Button";
 import strings from "../contants/strings";
 import {connect} from 'react-redux';
@@ -12,8 +12,100 @@ import colors from "../contants/colors";
 const icon = require('../assets/job_finder.png');
 let source = resolveAssetSource(icon);
 const SCREEN_WIDTH = getScreenWidth();
+const BUTTONS_INITIAL_WIDTH = SCREEN_WIDTH * 0.6;
+const BUTTONS_MAX_WIDTH = SCREEN_WIDTH - 2 * 20;
 
 class AuthScreen extends Component {
+
+    constructor() {
+        super();
+
+        this.buttonWith = new Animated.Value(BUTTONS_INITIAL_WIDTH);
+    }
+
+    componentDidMount() {
+        Animated.spring(this.buttonWith, {
+            toValue: BUTTONS_MAX_WIDTH,
+            duration: 2000
+        }).start(() => {
+        })
+    }
+
+    render(){
+        const {desiredWidth, desiredHeight} = this.generateSource();
+        const {
+            rootContainerStyle,
+            cardContainerStyle,
+            overlayViewContainer,
+            infoTextStyle,
+            imageStyle,
+            facebookButtonStyle,
+            googleButtonStyle,
+            googleButtonTextStyle,
+            emailButtonStyle
+        } = styles;
+
+        const buttonsContainerViewStyle = {
+            width: this.buttonWith
+        };
+
+        return (
+            <View style={rootContainerStyle}>
+
+                <View style={cardContainerStyle}>
+
+                    <View style={overlayViewContainer}>
+                        <Image source={require('../assets/hey.jpg')}
+                               style={{width: 50, height: 50}}/>
+                    </View>
+
+                    <Text style={infoTextStyle}>
+                        {strings.loginScreenText}
+                    </Text>
+
+                </View>
+
+                <Image source={{uri: source.uri}}
+                       style={[imageStyle, {width: desiredWidth, height: desiredHeight}]}/>
+
+                <Animated.View style={buttonsContainerViewStyle}>
+
+                    <Button
+                        onClick={this.onLoginWithFacebookButtonClicked}
+                        buttonText={strings.loginWithFacebook}
+                        buttonStyle={facebookButtonStyle}
+                        imageIcon={require('../assets/facebook_icon.png')}/>
+
+                    <Button
+                        onClick={this.onLoginWithGoogleButtonClicked}
+                        buttonText={strings.loginWithGoogle}
+                        buttonStyle={googleButtonStyle}
+                        buttonTextStyle={googleButtonTextStyle}
+                        imageIcon={require('../assets/google_icon.png')}/>
+
+                    <Button
+                        onClick={this.onLoginWithEmailButtonClicked}
+                        buttonText={strings.loginWithEmail}
+                        buttonStyle={emailButtonStyle}
+                        imageIcon={require('../assets/email_icon.png')}/>
+
+                </Animated.View>
+
+            </View>
+        );
+    }
+
+    onLoginWithFacebookButtonClicked = () => {
+        this.props.loginWithFacebook()
+    };
+
+    onLoginWithGoogleButtonClicked = () => {
+        //this.props.loginWithFacebook()
+    };
+
+    onLoginWithEmailButtonClicked = () => {
+        //this.props.loginWithFacebook()
+    };
 
     generateSource = () => {
         let width = source.width;
@@ -40,42 +132,6 @@ class AuthScreen extends Component {
 
         return {desiredWidth, desiredHeight};
     };
-
-    render(){
-        const {desiredWidth, desiredHeight} = this.generateSource();
-        const {rootContainerStyle, cardContainerStyle, overlayViewContainer, infoTextStyle, imageStyle, loginFacebookButtonStyle} = styles;
-
-        return (
-            <View style={rootContainerStyle}>
-
-                <View style={cardContainerStyle}>
-
-                    <View style={overlayViewContainer}>
-                        <Image source={require('../assets/hey.jpg')}
-                               style={{width: 50, height: 50}}/>
-                    </View>
-
-                    <Text style={infoTextStyle}>
-                        {strings.loginScreenText}
-                    </Text>
-
-                </View>
-
-                <Image source={{uri: source.uri}}
-                       style={[imageStyle, {width: desiredWidth, height: desiredHeight}]}/>
-
-                <Button
-                    onClick={this.onLoginWithFacebookButtonClicked}
-                    buttonText={strings.loginWithFacebook}
-                    buttonStyle={loginFacebookButtonStyle}/>
-
-            </View>
-        );
-    }
-
-    onLoginWithFacebookButtonClicked = () => {
-        this.props.loginWithFacebook()
-    }
 
 }
 
@@ -130,14 +186,42 @@ const styles = {
             height: 0.8
         },
     },
-    loginFacebookButtonStyle: {
+    facebookButtonStyle: {
+        width: '100%',
+        backgroundColor: colors.facebookButtonColor,
         shadowColor: '#3366ff',
         shadowOpacity: 0.6,
         shadowOffset: {
             width: 0.6,
             height: 0.8
         },
-    }
+    },
+    googleButtonStyle: {
+        backgroundColor: colors.white,
+        shadowColor: colors.black,
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 0.4,
+            height: 0.4
+        },
+        marginTop: 15,
+    },
+    googleButtonTextStyle: {
+        color: colors.googleButtonTextColor
+    },
+    emailButtonStyle: {
+        backgroundColor: colors.emailButtonColor,
+        shadowColor: colors.emailButtonColor,
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 0.4,
+            height: 0.4
+        },
+        marginTop: 15,
+    },
+    emailButtonTextStyle: {
+        color: colors.white
+    },
 };
 
 export default connect(null, {loginWithFacebook})(AuthScreen);
